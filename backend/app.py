02af0606ed904a8921cfb8ba6a451a0c8928802a
unknown <genekim@tecace.com>
2025-05-29 15:41:17 -0700

    changed host in app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -37,6 +37,6 @@ if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5000))
     
     # Get host from environment or use localhost for local dev
-    host = os.environ.get("HOST", "127.0.0.1")
+    host = os.environ.get("HOST", "0.0.0.0")
     
     app.run(host=host, port=port, debug=True)
