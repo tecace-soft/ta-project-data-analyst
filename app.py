8058e0326395222dac93ceaad1dedf409855f45e
unknown <genekim@tecace.com>
2025-06-05 13:11:25 -0700

    prep for Render deployment

--- app.py
+++ app.py
@@ -536,4 +536,7 @@ def chat_proxy():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000) 
+    # Get port from environment variable for deployment platforms like Render
+    port = int(os.environ.get('PORT', 5000))
+    # Set debug=False for production
+    app.run(host='0.0.0.0', port=port, debug=False) 
